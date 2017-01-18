c3eb0c57db54d40b6d261d5d837ac1ae69e01bda
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-01-18 15:17:54 +0800

    Updated the the package verino to the next version to prevent conflict

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ from codecs import open
 from os import path
 
 setup(
-        name='testpython',
+    name='testpython',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',
+    version='1.3.0',
 
     description='A sample Python project',
     long_description='Testing long description in a python project',
