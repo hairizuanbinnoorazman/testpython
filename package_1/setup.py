6e91baf78593e84b4c2975c80ff9fb87dddc8a5a
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-02-06 08:30:52 +0800

    chore: Renaming folder changes
    Remove undesirable names such as lol etc.

--- package_1/setup.py
+++ package_1/setup.py
@@ -11,7 +11,7 @@ from codecs import open
 from os import path
 
 setup(
-    name='lol',
+    name='package_1',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
